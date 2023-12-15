9013ff3e1bc698b7e9cff1bdd6119dda6ea1576b
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-12-15 16:26:23 +0800

    chore: print event

--- src/main.rs
+++ src/main.rs
@@ -215,33 +215,38 @@ fn main() {
         .with_size((400, 400))
         .with_anchor(Anchor::Left)
         .with_keyboard_interacivity(zwlr_layer_surface_v1::KeyboardInteractivity::Exclusive);
-    ev.running(|event, ev| match event {
-        Event::RequestBuffer(file, shm, qh, init_w, init_h) => {
-            draw(file, (init_w, init_h));
-            let pool = shm.create_pool(file.as_fd(), (init_w * init_h * 4) as i32, qh, ());
-            ReturnData::WlBuffer(pool.create_buffer(
-                0,
-                init_w as i32,
-                init_h as i32,
-                (init_w * 4) as i32,
-                wl_shm::Format::Argb8888,
-                qh,
-                (),
-            ))
-        }
-        Event::RequestMessages(DispatchMessage::Button { .. }) => {
-            ev.set_anchor(Anchor::Right);
-            ReturnData::None
-        }
-        Event::RequestMessages(DispatchMessage::KeyBoard { key, .. }) => {
-            if *key == 1 {
-                return ReturnData::RequestExist;
+
+    ev.running(|event, ev| {
+        println!("{:?}", event);
+        match event {
+            Event::RequestBuffer(file, shm, qh, init_w, init_h) => {
+                draw(file, (init_w, init_h));
+                let pool = shm.create_pool(file.as_fd(), (init_w * init_h * 4) as i32, qh, ());
+                ReturnData::WlBuffer(pool.create_buffer(
+                    0,
+                    init_w as i32,
+                    init_h as i32,
+                    (init_w * 4) as i32,
+                    wl_shm::Format::Argb8888,
+                    qh,
+                    (),
+                ))
             }
-            ReturnData::None
+            Event::RequestMessages(DispatchMessage::Button { .. }) => {
+                ev.set_anchor(Anchor::Right);
+                ReturnData::None
+            }
+            Event::RequestMessages(DispatchMessage::KeyBoard { key, .. }) => {
+                if *key == 1 {
+                    return ReturnData::RequestExist;
+                }
+                ReturnData::None
+            }
         }
     });
 }
 
+#[derive(Debug)]
 enum Event<'a> {
     RequestBuffer(
         &'a mut File,
