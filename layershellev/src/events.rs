ff8b52b5c2a126c97b662afec6ce9dba5433dc5b
Vitalii Lukyanov <lukyanov@xparvision.com>
2024-09-28 12:44:58 +0200

    unknown lints on stable, so removing

--- layershellev/src/events.rs
+++ layershellev/src/events.rs
@@ -109,7 +109,6 @@ impl Default for NewLayerShellSettings {
     }
 }
 
-#[allow(clippy::too_long_first_doc_paragraph)]
 /// the return data
 /// Note: when event is RequestBuffer, you must return WlBuffer
 /// Note: when receive InitRequest, you can request to bind extra wayland-protocols. this time you
