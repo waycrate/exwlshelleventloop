fc4554c66c665dc8cc88977321ccd7771f0474bb
sundaram123krishnan <krishnsundaram@gmail.com>
2025-03-08 18:53:41 +0530

    fix: layershell compositor screenshot

--- iced_layershell/src/application.rs
+++ iced_layershell/src/application.rs
@@ -722,12 +722,11 @@ pub(crate) fn run_action<A, C>(
             WindowAction::Screenshot(_id, channel) => {
                 let bytes = compositor.screenshot(
                     renderer,
-                    surface,
                     state.viewport(),
                     state.background_color(),
                     &debug.overlay(),
                 );
-                let _ = channel.send(window::Screenshot::new(
+                let _ = channel.send(iced_core::window::Screenshot::new(
                     bytes,
                     state.physical_size(),
                     state.scale_factor(),
