4d52a799f95b37882bf8e7be63b99e71eaf40df0
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2025-06-27 11:49:53 +0900

    chore: clippy fix

--- iced_examples/multi_window/src/main.rs
+++ iced_examples/multi_window/src/main.rs
@@ -192,7 +192,7 @@ impl Example {
 impl Window {
     fn new(count: usize) -> Self {
         Self {
-            title: format!("Window_{}", count),
+            title: format!("Window_{count}"),
             scale_input: "1.0".to_string(),
             current_scale: 1.0,
             theme: Theme::ALL[count % Theme::ALL.len()].clone(),
