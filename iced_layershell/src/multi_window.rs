waycrate/exwlshelleventloop

commit 7a4033cc3e542cc183456888b54ce31c446674ce

fix: program will froze
resolve: https://github.com/waycrate/exwlshelleventloop/issues/209

when request the refresh event, we need to reset the
present_available_state, and after the request done ,reset it.

iced_layershell/src/multi_window.rs
@@ -809,7 +809,6 @@ where
             return;
         }
 
-        let mut uis_stale = false;
         let mut rebuilds = Vec::new();
         for (iced_id, window) in self.window_manager.iter_mut() {
             let interact_span = debug::interact(iced_id);
@@ -845,7 +844,6 @@ where
             #[cfg(not(feature = "unconditional-rendering"))]
             let unconditional_rendering = false;
             if Self::handle_ui_state(ev, window, ui_state, unconditional_rendering, false) {
-                uis_stale = true;
                 rebuilds.push((iced_id, window));
             }
 
@@ -860,7 +858,7 @@ where
             interact_span.finish();
         }
 
-        if !self.messages.is_empty() || uis_stale {
+        if !self.messages.is_empty() {
             ev.request_refresh_all(RefreshRequest::NextFrame);
             let (caches, application) = self.user_interfaces.extract_all();
 

iced_sessionlock/src/multi_window.rs
@@ -522,7 +522,6 @@ where
             return;
         }
 
-        let mut uis_stale = false;
         let mut rebuilds = Vec::new();
         for (iced_id, window) in self.window_manager.iter_mut() {
             let interact_span = debug::interact(iced_id);
@@ -558,7 +557,6 @@ where
             #[cfg(not(feature = "unconditional-rendering"))]
             let unconditional_rendering = false;
             if Self::handle_ui_state(ev, window, ui_state, unconditional_rendering) {
-                uis_stale = true;
                 rebuilds.push((iced_id, window));
             }
 
@@ -573,7 +571,7 @@ where
             interact_span.finish();
         }
 
-        if !self.messages.is_empty() || uis_stale {
+        if !self.messages.is_empty() {
             ev.request_refresh_all(RefreshRequest::NextFrame);
             let (caches, application) = self.user_interfaces.extract_all();
 

layershellev/src/lib.rs
@@ -747,6 +747,8 @@ impl<T> WindowStateUnit<T> {
     }
 
     pub fn request_refresh(&mut self, request: RefreshRequest) {
+        // NOTE: if request directly, the state will always be available
+        self.present_available_state = PresentAvailableState::Available;
         // refresh request in nearest future has the highest priority.
         match self.request_flag.refresh {
             RefreshRequest::NextFrame => {}
@@ -3292,7 +3294,9 @@ impl<T: 'static> WindowState<T> {
                             // don't refresh, if size is 0.
                             continue;
                         }
-                        if unit.take_present_slot() {
+                        let take_present_slot = unit.take_present_slot();
+                        if take_present_slot {
+                            unit.reset_present_slot();
                             let unit_id = unit.id;
                             let is_created = unit.becreated;
                             let scale_float = unit.scale_float();

sessionlockev/src/lib.rs
@@ -408,6 +408,8 @@ impl<T> WindowStateUnit<T> {
     /// this function will refresh whole surface. it will reattach the buffer, and damage whole,
     /// and final commit
     pub fn request_refresh(&mut self, request: RefreshRequest) {
+        // NOTE: if request directly, the state will always be available
+        self.present_available_state = PresentAvailableState::Available;
         // refresh request in nearest future has the highest priority.
         match self.refresh {
             RefreshRequest::NextFrame => {}
@@ -1733,7 +1735,9 @@ impl<T: 'static> WindowState<T> {
                             // don't refresh, if size is 0.
                             continue;
                         }
-                        if unit.take_present_slot() {
+                        let take_present_slot = unit.take_present_slot();
+                        if take_present_slot {
+                            unit.reset_present_slot();
                             let unit_id = unit.id;
                             let scale_float = unit.scale_float();
                             let wl_surface = unit.wl_surface.clone();

commit 55fdd77af335b9e3b9d7185fda00f3731436a259

fix: #209, present slot may not be used after present, reset it and fix regression introduced in 7a4033cc3e
Signed-off-by: fortime <palfortime@gmail.com>

iced_layershell/src/multi_window.rs
@@ -525,8 +525,6 @@ where
                     panic!("{error:?}");
                 }
                 _ => {
-                    // In case of `ev.request_next_present` isn't been called. Reset present_slot.
-                    ev.reset_present_slot(layer_shell_id);
                     tracing::error!("Error {error:?} when presenting surface.");
                 }
             },

iced_sessionlock/src/multi_window.rs
@@ -432,8 +432,6 @@ where
                     panic!("{error:?}");
                 }
                 _ => {
-                    // In case of `ev.request_next_present` isn't been called. Reset present_slot.
-                    ev.reset_present_slot(session_lock_id);
                     tracing::error!("Error {error:?} when presenting surface.");
                 }
             },

layershellev/src/lib.rs
@@ -747,8 +747,6 @@ impl<T> WindowStateUnit<T> {
     }
 
     pub fn request_refresh(&mut self, request: RefreshRequest) {
-        // NOTE: if request directly, the state will always be available
-        self.present_available_state = PresentAvailableState::Available;
         // refresh request in nearest future has the highest priority.
         match self.request_flag.refresh {
             RefreshRequest::NextFrame => {}
@@ -787,8 +785,6 @@ impl<T> WindowStateUnit<T> {
 
     pub fn reset_present_slot(&mut self) -> bool {
         if self.present_available_state == PresentAvailableState::Taken {
-            // refresh at next frame.
-            self.request_flag.refresh = RefreshRequest::NextFrame;
             self.present_available_state = PresentAvailableState::Available;
             true
         } else {
@@ -3294,9 +3290,7 @@ impl<T: 'static> WindowState<T> {
                             // don't refresh, if size is 0.
                             continue;
                         }
-                        let take_present_slot = unit.take_present_slot();
-                        if take_present_slot {
-                            unit.reset_present_slot();
+                        if unit.take_present_slot() {
                             let unit_id = unit.id;
                             let is_created = unit.becreated;
                             let scale_float = unit.scale_float();
@@ -3326,6 +3320,8 @@ impl<T: 'static> WindowState<T> {
                                 }),
                                 Some(unit_id),
                             );
+                            // reset if the slot is not used
+                            window_state.units[idx].reset_present_slot();
                         }
                     }
                     TimeoutAction::ToDuration(std::time::Duration::from_millis(50))

sessionlockev/src/lib.rs
@@ -408,8 +408,6 @@ impl<T> WindowStateUnit<T> {
     /// this function will refresh whole surface. it will reattach the buffer, and damage whole,
     /// and final commit
     pub fn request_refresh(&mut self, request: RefreshRequest) {
-        // NOTE: if request directly, the state will always be available
-        self.present_available_state = PresentAvailableState::Available;
         // refresh request in nearest future has the highest priority.
         match self.refresh {
             RefreshRequest::NextFrame => {}
@@ -448,8 +446,6 @@ impl<T> WindowStateUnit<T> {
 
     pub fn reset_present_slot(&mut self) -> bool {
         if self.present_available_state == PresentAvailableState::Taken {
-            // refresh at next frame.
-            self.refresh = RefreshRequest::NextFrame;
             self.present_available_state = PresentAvailableState::Available;
             true
         } else {
@@ -1735,9 +1731,7 @@ impl<T: 'static> WindowState<T> {
                             // don't refresh, if size is 0.
                             continue;
                         }
-                        let take_present_slot = unit.take_present_slot();
-                        if take_present_slot {
-                            unit.reset_present_slot();
+                        if unit.take_present_slot() {
                             let unit_id = unit.id;
                             let scale_float = unit.scale_float();
                             let wl_surface = unit.wl_surface.clone();
@@ -1770,6 +1764,8 @@ impl<T: 'static> WindowState<T> {
                                 ),
                                 Some(unit_id),
                             );
+                            // reset if the slot is not used
+                            window_state.units[idx].reset_present_slot();
                         }
                     }
                     TimeoutAction::ToDuration(Duration::from_millis(50))