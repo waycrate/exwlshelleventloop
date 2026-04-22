d46296c0e340c67222f58de2fa210b1de20c0637
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2026-04-22 16:35:02 +0900

    refactor: clean up waycrate_xkbkeycode
    reuse the object and definition from winit-common and winit-core

--- waycrate_xkbkeycode/src/keyboard.rs
+++ waycrate_xkbkeycode/src/keyboard.rs
@@ -1,7 +1,5 @@
 //! Types related to the keyboard.
 
-pub use smol_str::SmolStr;
-
 pub use winit_core::keyboard::{
     Key, KeyCode, KeyLocation, ModifiersState, NamedKey, NativeKey, NativeKeyCode, PhysicalKey,
 };
