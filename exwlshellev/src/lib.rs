94503d52930596cbea4ea3647f8e756a53df5bd1
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2026-09-21 21:38:52 +0900

    chore: fix the unit test of document

--- exwlshellev/src/lib.rs
+++ exwlshellev/src/lib.rs
@@ -69,6 +69,20 @@
 //!    }
 //!
 //!    fn on_normal_dispatch(&mut self, _state: &mut WindowState<()>) {}
+//!    fn on_refresh(
+//!        &mut self,
+//!        state: &mut WindowState<()>,
+//!        _looph: &calloop::LoopHandle<'static, EventContext<(), Self>>,
+//!        id: id::Id,
+//!    ) {
+//!        let Some(ex_wlshell_window) = state.get_unit_with_id(id) else {
+//!            return;
+//!        };
+//!
+//!        let (width, height) = ex_wlshell_window.get_size();
+//!
+//!        println!("{width}, {height}");
+//!    }
 //!    fn on_event(
 //!        &mut self,
 //!        state: &mut WindowState<()>,
@@ -76,13 +90,6 @@
 //!        _id: Option<id::Id>,
 //!    ) {
 //!        match event {
-//!            ExWlShellEvent::RequestRefresh {
-//!                width,
-//!                height,
-//!                ..
-//!            } => {
-//!                println!("{width}, {height}");
-//!            }
 //!            ExWlShellEvent::MouseEnter { pointer, .. } => state
 //!                .push_request(Request::RequestSetCursor((
 //!                    Cursor::Shape(CursorShape::Crosshair),
